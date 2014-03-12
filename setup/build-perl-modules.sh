f2187a7a93e2f601cb8f5d9485c439f12b530410
Roman Plessl <roman.plessl@oetiker.ch>
2014-03-12 15:53:41 +0100

    added Term::ReadKey (missing on RHEL 6)

--- setup/build-perl-modules.sh
+++ setup/build-perl-modules.sh
@@ -3,11 +3,12 @@
 . `dirname $0`/sdbs.inc
 
 for module in \
-IO::Socket::SSL \
-Mail::POP3Client \
-DBI \
-DBD::SQLite \
-Mojolicious \
+  Term::ReadKey \
+  IO::Socket::SSL \
+  Mail::POP3Client \
+  DBI \
+  DBD::SQLite \
+  Mojolicious \
 ; do
   perlmodule $module
 done
